7674ef027f17e56fe530b4b5b16417cb57ebd804
mkiiim7 <154470894+mkiiim7@users.noreply.github.com>
2024-11-25 17:18:19 -0300

    GEmSound

--- Assets/Scripst/Gem.cs
+++ Assets/Scripst/Gem.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 
 public class Gem : MonoBehaviour , Item
 {
+    [SerializeField] private AudioSource soundGem;
 
     public static event Action<int> OnGemCollect;
     public int worth = 5;
+
+    private void Awake()
+    {
+        soundGem = FindAnyObjectByType<AudioSource>();
+    }
     public void Collect()
     {
         OnGemCollect.Invoke(worth);
+        soundGem.Play();
         Destroy(gameObject);
     }
 
